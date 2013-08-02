71d30cdbb42037e25cc02fca64067c994d9c3c0c
Glenn Randers-Pehrson <glennrp at users.sourceforge.net>
2013-08-02 10:29:41 -0500

    [ngcrush] Bump version to 1.7.67

--- pngcrush.h
+++ pngcrush.h
@@ -5,7 +5,7 @@
  * license (see LICENSE, in pngcrush.c).
  */
 
-/* Special defines for pngcrush version 1.7.66 */
+/* Special defines for pngcrush version 1.7.67 */
 
 #ifndef PNGCRUSH_H
 #define PNGCRUSH_H
